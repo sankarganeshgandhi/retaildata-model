68783010efa8e85dc6f6c16fb1622c5e45b89164
Sankarganesh Gandhi <sankarganesh.gandhi@happiestminds.com>
2018-08-15 16:03:06 +0100

    Initial draft version of the model classes for retail data project

---
+++ src/main/java/com/sgglabs/retail/App.java
@@ -0,0 +1,10 @@
+package com.sgglabs.retail;
+
+/**
+ * Retail Data Scanning and Analysis Project
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Retail data");
+    }
+}
